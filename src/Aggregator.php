5cc77bebdd8f74dd8dc1ed56ad4308abe7369ae7
KaniRobinson <me@kanirobinson.co.uk>
2020-09-13 22:42:58 +0100

    Initial commit

---
+++ src/Aggregator.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Aggregators\Electron;
+
+use Carbon\Carbon;
+use InvalidArgumentException;
+use Aggregators\Support\BaseAggregator;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Aggregator extends BaseAggregator
+{
+    /**
+     * {@inheritDoc}
+     */
+    public string $uri = 'https://www.electronjs.org/blog';
+
+    /**
+     * {@inheritDoc}
+     */
+    public string $provider = 'Electron';
+
+    /**
+     * {@inheritDoc}
+     */
+    public string $logo = 'logo.png';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function articleIdentifier(): string
+    {
+        return 'ul.posts-list > li';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function nextUrl(Crawler $crawler): ?string
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function image(Crawler $crawler): ?string
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function title(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('a.f1-light.blog-index-link.mr-2')->text();
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function content(Crawler $crawler): ?string
+    {
+        try {
+            return $crawler->filter('p:first-of-type')->text();
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function link(Crawler $crawler): ?string
+    {
+        try {
+            return 'https://www.electronjs.org' . $crawler->filter('a.f1-light.blog-index-link.mr-2')->attr('href');
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dateCreated(Crawler $crawler): Carbon
+    {
+        try {
+            return Carbon::parse($crawler->filter('span[data-date]')->attr('data-date'));
+        } catch (InvalidArgumentException $e) {
+            return Carbon::now();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function dateUpdated(Crawler $crawler): Carbon
+    {
+        return $this->dateCreated($crawler);
+    }
+}
